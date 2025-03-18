8581c333d82b14539ef7a3bd5dc5040426bded97
AKS-Design004 <thiamabdoukarim89@gmail.com>
2025-03-18 17:04:23 +0000

    Update deploy.sh

--- labisi2025/labisi2025/deploy.sh
+++ labisi2025/labisi2025/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-CONTAINER_NAME=" utilisateur/labisi2025"
-IMAGE_NAME=" utilisateur/labisi2025:latest"
+CONTAINER_NAME=" abdou004/labisi2025"
+IMAGE_NAME=" abdou004/labisi2025:latest"
 
 # Arrêt du conteneur existant
 sudo docker stop $CONTAINER_NAME || true
